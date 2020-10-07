0fcecbb368d691a48c6a431b8f84b63c122728af
thom <you@example.com>
2020-10-07 15:02:57 +0200

    countdown toegevoegd

---
+++ countdown.py
@@ -0,0 +1,15 @@
+for i in range(1000, -1, -1):
+    print(i)
+
+if i == 0:
+    print(". 　　　。　　　　•　    　ﾟ　　。 　　.")
+    print("")
+    print("　　　.　　　  　　.　　　　　。　　   。　.  　")
+    print("")
+    print(".　　      。　　　　　 ඞ   。   . 　　 • 　　　　•")
+    print("")
+    print("　　ﾟ　　 sletta was not An Impostor.　 。　.")
+    print("")
+    print("　　'　　　    1 Impostor remains 　 　　。")
+    print("")
+    print("　　ﾟ　　　.　　　. ,　　　　.　 .")
