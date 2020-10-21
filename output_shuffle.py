443c96e6cfd381fd52d1eda797780306fdc3e54d
thom <you@example.com>
2020-10-21 15:10:16 +0200

    de output van me shuffle

---
+++ output_shuffle.py
@@ -0,0 +1,8 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\SKIKK\mediacollege amsterdam\bewijzenmap\periode 1.1\programmeren in python\pythonachievements\shuffle_and_return.py
+ogeotr
+odre
+apa
+>>> 
