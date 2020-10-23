d18f25bdf8b04af64487c463f294b0feaeb3d78e
thom <you@example.com>
2020-10-23 11:44:57 +0200

    les06 achievement afgemaakt

---
+++ PYTB1L6CanYouHandleTheList.py
@@ -0,0 +1,8 @@
+lijstje1 = [1, True, "hallo", 3566, "twintig"]
+print(lijstje1)
+lijstje1.append("gert")
+print(lijstje1)
+lijstje1.remove("hallo")
+print(lijstje1)
+lijstje1.insert(2, 69)
+print(lijstje1)
